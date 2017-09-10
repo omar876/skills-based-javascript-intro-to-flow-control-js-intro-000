3969915c7ff107a06ab758b6ac13df8bdf33d9e5
omar Dinnall <omarradcliffe3@gmail.com>
2017-09-10 20:20:13 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -35,10 +35,10 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch(age) {
     case age >=13 && age <=19 :
-      console.log( "You are a teenager");
+      console.log("You are a teenager");
       break;
     default:
-      return "You are a teenager";
+      return "You have an age";
       break;
   }
 //return "You have an age" as the default.
