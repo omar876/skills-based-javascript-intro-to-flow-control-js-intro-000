a7b060513d8ecf9390aafe52b3d114bad1ec7066
omar Dinnall <omarradcliffe3@gmail.com>
2017-09-10 20:01:59 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -36,7 +36,7 @@ function switchAge(age) {
   switch(age) {
     case age >=13 && age <=19 :
       return "You are a teenager"
-    default: 
+    default:
       return "You have an age"
   }
 //return "You have an age" as the default.
